94f98323664b9af0756a527fd490516d10e35f61
Luis Ricardo Sanchez <lsanchez@esferasoluciones.com>
2019-08-14 19:43:32 -0500

    prueba

--- platforms/android/app/src/main/java/com/esferasoluciones/mymat_light_deutsch/MainActivity.java
+++ platforms/android/app/src/main/java/com/esferasoluciones/mymat_light_deutsch/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package com.esferasoluciones.mymatgr;
+package com.esferasoluciones.mymat_light_deutsch;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
